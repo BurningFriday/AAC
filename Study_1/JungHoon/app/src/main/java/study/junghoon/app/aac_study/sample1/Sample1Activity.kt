a5652ea0f2d60584765edb4f2f07bd2917e6be98
parkjunghoon <go9018@naver.com>
2020-02-09 20:41:30 +0900

    add : activity use databinding

--- Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/sample1/Sample1Activity.kt
+++ Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/sample1/Sample1Activity.kt
@@ -3,13 +3,20 @@ package study.junghoon.app.aac_study.sample1
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import study.junghoon.app.aac_study.R
+import study.junghoon.app.aac_study.databinding.ActivitySample1Binding
 
 class Sample1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample1)
+//        setContentView(R.layout.activity_sample1)
+        val binding : ActivitySample1Binding=
+            DataBindingUtil.setContentView(this, R.layout.activity_sample1)
+
+        binding.name = "Your name"
+        binding.lastName = "Your last name"
     }
 
     fun onLike(view: View) {}
