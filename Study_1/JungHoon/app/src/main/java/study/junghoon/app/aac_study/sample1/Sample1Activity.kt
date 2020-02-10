4c53bf990908927035e22b8c14acfad68c3f3028
parkjunghoon <go9018@naver.com>
2020-02-10 21:21:32 +0900

    fix : databinding with viewmodel

--- Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/sample1/Sample1Activity.kt
+++ Study_1/JungHoon/app/src/main/java/study/junghoon/app/aac_study/sample1/Sample1Activity.kt
@@ -1,13 +1,16 @@
 package study.junghoon.app.aac_study.sample1
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import study.junghoon.app.aac_study.R
 import study.junghoon.app.aac_study.databinding.ActivitySample1Binding
+import study.junghoon.app.aac_study.plain.data.SimpleViewModel
 
 class Sample1Activity : AppCompatActivity() {
+
+    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +18,9 @@ class Sample1Activity : AppCompatActivity() {
         val binding : ActivitySample1Binding=
             DataBindingUtil.setContentView(this, R.layout.activity_sample1)
 
-        binding.name = "Your name"
-        binding.lastName = "Your last name"
+        /**
+         * 이를 통해 이전 액티비티에서 메서드를 통해 초기화했던 부분을 생략할 수 있다.
+         */
+        binding.viewmodel = viewModel
     }
-
-    fun onLike(view: View) {}
 }
